4eadf872a6c1630f2320da4973a3fba597b5fe69
Kaine750 <64343502+Kaine750@users.noreply.github.com>
2022-03-16 16:17:10 +0900

    Update chank_load.cpp

--- chank_load.cpp
+++ chank_load.cpp
@@ -1,17 +1,18 @@
 #include <SD.h>
 #include <SPI.h>
 #include <Wire.h>
+#define rep(i,n) for(int i = 0;i <(int)n;i++)
 
 
 const int CS = 4;
 
 void setup() {
     Serial.begin(9600); //シリアル通信開始
-    Serial.println("Initializing SD.....");
+    Serial.println("Initializing SD.....");//"Initializing SD....."と表示
     pinMode(10, OUTPUT);
 
     if (!SD.begin(CS)) {
-        Serial.println("not found SD");
+        Serial.println("not found SD");//SDカードが軌道できなかったら表示
     } else {
         Serial.println("SD OK");
     }
@@ -24,7 +25,7 @@ void loop(char length, char width) {
             datachar[i][j] = field_map[i][j];
         }
     }
-    File dataFile = SD.open("log.txt", FILE_WRITE);
+    File dataFile = SD.open("log.txt", FILE_WRITE); //"log.txt"にマップを入れる
     
     if (dataFile) {
         Serial.println(datachar); //シリアルコンソールに値を表示
@@ -36,13 +37,13 @@ void loop(char length, char width) {
     delay(60000);
 }
 
-void importing(char chank_imp[4][11][11], short int cur_x, short int cur_y, short int rel_x, short int rel_y){
-    File dataFile = SD.open("log.txt");
+void importing(char chunk_imp[4][11][11], short int cur_x, short int cur_y, short int rel_x, short int rel_y){ //SDカードに[4][11][11]のチャンクを読み込み
+    File dataFile = SD.open("log.txt");                                                                        //(cur_x,cur_y)は現在地,(rel_x,rel_y)は相対座標
     if(dataFile){
         while(dataFile.available()){
             Serial.write(dataFile.read());
         }
-        rep(i, 11){
+        rep(i, 11){                                                                                            //点・線対象に読み込み
             rep(j, 11){
                 dataFile[i + cur_x + rel_x][j + cur_y + rel_y].println(chank_imp[0][i][j]);}}
         for(short int i = 0;i > -11;i--){
@@ -61,28 +62,28 @@ void importing(char chank_imp[4][11][11], short int cur_x, short int cur_y, shor
 delay(2000);
 }
 
-void chank_upload(char chank_map[4][11][11], short int cur_x, short int cur_y, short int rel_x, short int rel_y){
-    const short int x_limit = 100;
-    const short int y_limit = 50;
+void chunk_upload(char chunk_map[4][11][11], short int cur_x, short int cur_y, short int rel_x, short int rel_y){ //[4][11][11]のチャンクにSDカードから書き込み
+    const short int x_limit = 100;                                                                                //(cur_x,cur_y)は現在地,(rel_x,rel_y)は相対座標
+    const short int y_limit = 50;                                                                                 //limit_x,limit_yを超えると0(未探索)を返す
     File dataFile = SD.open("log.txt");
     if(dataFile){
         while(dataFile.available()){
             Serial.write(dataFile.read());
         }
-        //vector<vector<int> > chank_map(chank_num,vector<int>(chank_num,0));
+        //vector<vector<int> > chank_map(chunk_num,vector<int>(chunk_num,0));
         if(abs(11 + j + cur_x + rel_x) <= 100 && abs(11 + j + cur_y + rel_y) <= 50){  
             rep(i, 11){
                 rep(j, 11){
-                    chank_map[0][j][k] = dataFile[j + cur_x + rel_x][k + cur_y + rel_y];}}
+                    chunk_map[0][j][k] = dataFile[j + cur_x + rel_x][k + cur_y + rel_y];}}
             for(short int i = 0;i > -11;i--){
                 rep(j, 11){
-                    chank_map[1][j][k] = dataFile[j + cur_x - rel_x][k + cur_y + rel_y];}}
+                    chunk_map[1][j][k] = dataFile[j + cur_x - rel_x][k + cur_y + rel_y];}}
             for(short int i = 0;i > -11;i--){
                 for(short int j = 0;j > -11;j--){
-                    chank_map[2][j][k] = dataFile[j + cur_x - rel_x][k + cur_y - rel_y];}}
+                    chunk_map[2][j][k] = dataFile[j + cur_x - rel_x][k + cur_y - rel_y];}}
             rep(i, 11){
                 for(short int j = 0;j > -11;j--){         
-                    chank_map[3][j][k] = dataFile[j + cur_x + rel_x][k + cur_y - rel_y];}}
+                    chunk_map[3][j][k] = dataFile[j + cur_x + rel_x][k + cur_y - rel_y];}}
         }
         else{
                 chank_map[0][j][k] = 0;
@@ -99,7 +100,7 @@ void chank_upload(char chank_map[4][11][11], short int cur_x, short int cur_y, s
 }
 
 #include <math.h>
-void r_theta_table(unsigned char r_theta_map[2][15][15], short int rel_x_1, short int rel_y_1){
+void r_theta_table(unsigned char r_theta_map[2][15][15], short int rel_x_1, short int rel_y_1){ //rとthetaの表.与えられた相対座標のものを計算
     const float conv = 40.286; //(180 * 90 / M_PI / 128)
     rep(i, short int 15){
         rep(j, short int 15){
@@ -111,7 +112,7 @@ void r_theta_table(unsigned char r_theta_map[2][15][15], short int rel_x_1, shor
 }
 
 //PLANB
-void chank_11(char chank_map[22][22], short int cur_x, short int cur_y, short int rel_x, short int rel_y){
+void chunk_11(char chunk_map[22][22], short int cur_x, short int cur_y, short int rel_x, short int rel_y){ //[22][22]のチャンクをSDカードから書き込み
     const short int x_limit = 100;
     const short int y_limit = 50;
     File dataFile = SD.open("log.txt");
@@ -119,14 +120,14 @@ void chank_11(char chank_map[22][22], short int cur_x, short int cur_y, short in
         while(dataFile.available()){
             Serial.write(dataFile.read());
         }
-        //vector<vector<int> > chank_map(chank_num,vector<int>(chank_num,0));
+        //vector<vector<int> > chunk_map(chunk_num,vector<int>(chunk_num,0));
         if(abs(22 + j + cur_x + rel_x) <= 100 && abs(22 + j + cur_y + rel_y) <= 50){  
             rep(i, 22){
                 rep(j, 22){
-                    chank_map[j][k] = dataFile[j + cur_x + rel_x][k + cur_y + rel_y];}}
+                    chunk_map[j][k] = dataFile[j + cur_x + rel_x][k + cur_y + rel_y];}}
             //for(short int i = 0;i > -11;i--){
             //    rep(j, 11){
-            //        chank_map[1][j][k] = dataFile[j + cur_x - rel_x][k + cur_y + rel_y];}}
+            //        chunk_map[1][j][k] = dataFile[j + cur_x - rel_x][k + cur_y + rel_y];}}
             //for(short int i = 0;i > -11;i--){
             //    for(short int j = 0;j > -11;j--){
             //        chank_map[2][j][k] = dataFile[j + cur_x - rel_x][k + cur_y - rel_y];}}
@@ -135,7 +136,7 @@ void chank_11(char chank_map[22][22], short int cur_x, short int cur_y, short in
             //        chank_map[3][j][k] = dataFile[j + cur_x + rel_x][k + cur_y - rel_y];}}
         }
         else{
-                chank_map[j][k] = 0;
+                chunk_map[j][k] = 0;
                 //chank_map[1][j][k] = 0;
                 //chank_map[2][j][k] = 0;
                 //chank_map[3][j][k] = 0;
@@ -148,8 +149,8 @@ void chank_11(char chank_map[22][22], short int cur_x, short int cur_y, short in
     delay(2000);
 }
 
-void SD_read_map(char map_memory[12][5][5], short int X[12], short int Y[12]){ //X,Yは12 map_memoryは12*5*5
-    File dataFile = SD.open("log.txt");
+void SD_read_map(char map_memory[12][5][5], short int X[12], short int Y[12]){ //X,Yは[12],map_memoryは[12][5][5]
+    File dataFile = SD.open("log.txt");                                        //SDカードから読み込み
     if(dataFile){
         while(dataFile.available()){
             Serial.write(dataFile.read());
